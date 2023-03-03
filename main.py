6226babce216b379ba2232c14e881dbcb682f378
GuastavinoAgustina <agustinaguastavino@gmail.com>
2023-03-03 15:23:53 -0300

    p1

---
+++ main.py
@@ -0,0 +1,18 @@
+""""Script para probar el Pipeline"""
+
+
+def main():
+    """"Funcion principal"""
+    print("Iniciando programita de prueba")
+    # Creating the HTML file
+    with open("build/index.html", "w", encoding="utf-8") as file_html:
+        # Adding the input data to the HTML file
+        file_html.write('''<html>
+        <head>
+        <title>Pagina</title>
+        </head>
+        <body>
+        <h1>Welcome</h1>
+        <p>Nuevo archivo html</p>
+        </body>
+        </html>''')
