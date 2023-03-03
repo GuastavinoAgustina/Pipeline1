44050593a79d302d9516646eb1f61d5a46fc5640
GuastavinoAgustina <agustinaguastavino@gmail.com>
2023-03-03 16:25:29 -0300

    p8

--- cond.sh
+++ cond.sh
@@ -1,4 +1,4 @@
-CODE=$(curl -v --stderr deploy.log -w "%{http_code}\n" -F password=$1 -F file=@build/index.html -F user=$2 https://cs.uns.edu.ar/~jose.moyano/index.php)
+CODE=$(curl -v -w "%{http_code}\n" -F password=$1 -F file=@index.html -F user=$2 https://cs.uns.edu.ar/~jose.moyano/index.php)
 
 if [ $CODE -ne 200 ]
 then exit 1
